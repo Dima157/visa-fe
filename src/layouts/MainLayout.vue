c16a325068e6f3e5f3f214645187004f3e45ee33
Dima <sarnodima157@gmail.com>
2020-10-31 19:39:24 +0200

    init project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="main-layout">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainLayout"
+}
+</script>
