70820d37bbe89cfc7ba1f8fbdcd64d724fc2c3b7
Dima <sarnodima157@gmail.com>
2020-11-01 15:55:56 +0200

    remove vue formulate

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vueCountryRegionSelect from 'vue-country-region-select'
-import VueFormulate from '@braid/vue-formulate'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
-Vue.use(VueFormulate)
-Vue.use(vueCountryRegionSelect)
 
 Vue.config.productionTip = false
 
