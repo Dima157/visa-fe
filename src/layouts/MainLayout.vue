a78e2877a79efa58c4bb44fff25fa583032624d9
Yaroslav Melnychuk <yariksuperman95@gmail.com>
2020-11-03 21:27:22 +0200

    Add fake login button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,20 +3,29 @@
     <div class="page-container md-layout-column">
       <md-app>
         <md-app-toolbar class="md-primary">
-          <span class="md-title">VisaApp</span>
+          <router-link to="/">
+            <span class="md-title">VisaApp</span>
+          </router-link>
           <div class="md-toolbar-section-end">
-            <md-button @click="showSidepanel = true">
-              <md-avatar class="md-avatar-icon md-small">
-                <md-icon>home</md-icon>
-              </md-avatar>
+            <md-menu v-if="!currentUser">
+              <md-button class="md-icon-button md-dense md-primary" md-menu-trigger>
+                <md-icon>person</md-icon>
+              </md-button>
+              <md-menu-content>
+                <md-menu-item to="/sign-up">Sign up</md-menu-item>
+                <md-menu-item to="/" @click="currentUser = null" v-if="currentUser">Logout</md-menu-item>
+                <md-menu-item to="/" @click="currentUser = {name: 'Test User'}" v-else>Login</md-menu-item>
+              </md-menu-content>
+            </md-menu>
+            <md-button class="md-icon-button md-dense md-primary" @click="showSidepanel = true" v-if="currentUser">
+              <md-icon>person</md-icon>
             </md-button>
           </div>
         </md-app-toolbar>
-        <md-app-drawer class="md-right" :md-active.sync="showSidepanel">
+        <md-app-drawer class="md-right" :md-active.sync="showSidepanel" v-if="currentUser">
           <md-toolbar class="md-transparent" md-elevation="0">
-            <span class="md-title">Favorites</span>
+            <span class="md-title">{{currentUser.name}}</span>
           </md-toolbar>
-
           <md-list>
             <md-list-item v-for="rout in routes" @click="showSidepanel = false" :to="rout.link">
               <span class="md-list-item-text" >{{ rout.name }}</span>
@@ -41,6 +50,7 @@ export default {
   name: "MainLayout",
   data: () => ({
     showSidepanel: false,
+    currentUser: null,
     routes: [
       {link: '/sign-up', name: 'Sign up'},
       {link: '/', name: 'Login'}
