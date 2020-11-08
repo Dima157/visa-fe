a232009d2a6e5e981405e2ca9ef38ed6c1692c86
Sarno <d.sarno@spheremall.com>
2020-11-08 13:57:05 +0200

    add styles to registration and login forms

--- src/components/forms/SignInForm.vue
+++ src/components/forms/SignInForm.vue
@@ -1,14 +1,18 @@
 <template>
-  <form @submit.prevent="login()">
-    <md-field>
-      <md-input type="email" v-model="user.email"/>
-    </md-field>
-    <md-field>
-      <md-input type="password" v-model="user.password"/>
-    </md-field>
-
-    <md-button type="submit" :disabled="$v.$invalid">Save</md-button>
-
+  <form @submit.prevent="login()" class="md-layout md-alignment-top-center">
+    <md-card class="md-layout-item md-size-30 md-small-size-50">
+      <md-card-content>
+        <md-field>
+          <md-input type="email" v-model="user.email"/>
+        </md-field>
+        <md-field>
+          <md-input type="password" v-model="user.password"/>
+        </md-field>
+      </md-card-content>
+      <md-card-actions>
+        <md-button type="submit" :disabled="$v.$invalid">Save</md-button>
+      </md-card-actions>
+    </md-card>
   </form>
 </template>
 
--- src/components/forms/SignUpForm.vue
+++ src/components/forms/SignUpForm.vue
@@ -1,38 +1,54 @@
 <template>
-  <form @submit.prevent="submit()">
-    <md-field>
-      <label>Email</label>
-      <md-input type="email" validation="email" v-model="user.email" required/>
-    </md-field>
-    <md-field>
-      <label>Name</label>
-      <md-input type="text" v-model="user.name" required/>
-    </md-field>
-    <md-field>
-      <label>Last name</label>
-      <md-input type="text" v-model="user.lastName" required/>
-    </md-field>
-    <md-field>
-      <label>Country</label>
-      <md-select name="country" id="country" required v-model="user.country">
-        <md-option v-for="country in countries" :value="country.code">{{ country.name }}</md-option>
-      </md-select>
-    </md-field>
-    <md-field>
-      <label>Password</label>
-      <md-input type="password" name="password" required v-model="user.password"/>
-    </md-field>
-    <md-field>
-      <label>Password confirm</label>
-      <md-input type="password" v-model="user.password_confirm" name="password_confirm" required/>
-      <small
-          class="helper-text invalid"
-          v-if="$v.user.password_confirm.$invalid"
-      >
-        Пароли не совпадают
-      </small>
-    </md-field>
-    <md-button type="submit" :disabled="$v.$invalid">Save</md-button>
+  <form @submit.prevent="submit()" class="md-layout md-alignment-top-center">
+    <md-card class="md-layout-item md-size-50 md-small-size-100">
+      <md-card-header>
+        <div class="md-title">User registration</div>
+      </md-card-header>
+      <md-card-content>
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label>Name</label>
+              <md-input type="text" v-model="user.name" required/>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label>Last name</label>
+              <md-input type="text" v-model="user.lastName" required/>
+            </md-field>
+          </div>
+        </div>
+
+        <md-field>
+          <label>Email</label>
+          <md-input type="email" validation="email" v-model="user.email" required/>
+        </md-field>
+        <md-field>
+          <label>Country</label>
+          <md-select name="country" id="country" required v-model="user.country">
+            <md-option v-for="country in countries" :value="country.code">{{ country.name }}</md-option>
+          </md-select>
+        </md-field>
+        <md-field>
+          <label>Password</label>
+          <md-input type="password" name="password" required v-model="user.password"/>
+        </md-field>
+        <md-field>
+          <label>Password confirm</label>
+          <md-input type="password" v-model="user.password_confirm" name="password_confirm" required/>
+          <span
+              class="md-helper-text invalid"
+              v-if="$v.user.password_confirm.$invalid"
+          >
+            Пароли не совпадают
+          </span>
+        </md-field>
+      </md-card-content>
+      <md-card-actions>
+        <md-button type="submit" :disabled="$v.$invalid">Save</md-button>
+      </md-card-actions>
+    </md-card>
   </form>
 </template>
 
